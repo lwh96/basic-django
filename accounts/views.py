4e133fd3d9b160005036745bbc213fee3e0b615e
lwh96 <wenhong@live.com>
2021-08-29 16:55:46 +0800

    added views and endpoints for accounts

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
 
+from rest_framework import viewsets, generics
+from .serializers import AccountSerializer, RegisterSerializer
+from rest_framework.response import Response
+from .models import Account
 # Create your views here.
+
+
+class AccountViewSet(viewsets.ModelViewSet):
+    queryset = Account.objects.all()
+    serializer_class = AccountSerializer
+
+
+class RegisterApiView(generics.GenericAPIView):
+    serializer_class = RegisterSerializer
+
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data)
+        result = {}
+        if serializer.is_valid():
+            account = serializer.save()
+            print('account', account)
+            result["message"] = "Account has been successfully created"
+            result["email"] = account.email
+            result["username"] = account.username
+        else:
+            result = serializer.errors
+        print(result)
+        return Response(result)
