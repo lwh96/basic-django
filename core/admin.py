ca7fad4dcffdf1fa7590671cb25ee0ea507aff2d
lwh96 <wenhong@live.com>
2021-08-29 16:44:44 +0800

    updated admin.py

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Diamond, Rectangle, Square, Triangle
 
 # Register your models here.
+
+admin.site.register(Rectangle)
+admin.site.register(Square)
+admin.site.register(Diamond)
+admin.site.register(Triangle)
